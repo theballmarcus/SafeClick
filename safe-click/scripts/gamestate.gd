0463d8ce4743a7115b84b1bb88bc0b7699ef6dc0
Marcus <mcihme@gmail.com>
2026-04-15 14:46:20 +0200

    Get it workig

--- safe-click/scripts/gamestate.gd
+++ safe-click/scripts/gamestate.gd
@@ -1,7 +1,7 @@
 extends Node
 
-#const API_URL = "http://139.59.130.153:3000"
-const API_URL = "http://localhost:3000"  
+const API_URL = "http://rev.theballmarcus.dk:3000"
+#const API_URL = "http://localhost:3000"  
 
 const API_KEY = "x-api-key: megetUkendtAPiK4YsomAldrigBLIRFUNDET"
 var headers = ["Content-Type: application/json", API_KEY]

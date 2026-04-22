1b1c51ed02f00e86fa2f95092a525ab2b2fa4285
TheodorHolst <theodor@ugleholdt.com>
2026-04-22 15:12:24 +0200

    mere på tutorial

--- safe-click/scenes/control.gd
+++ safe-click/scenes/control.gd
@@ -2,20 +2,27 @@ extends Control
 
 @onready var speech_text: RichTextLabel = $SpeechText
 @onready var arrow = $Arrow
-@onready var afsender = $Afsender
-@onready var emne = $Emne
+@onready var afsender_indhold = $AfsenderIndhold
+@onready var emne_indhold = $EmneIndhold
+@onready var titel_mail = $TitelMail
+@onready var emne_mail = $EmneMail
+@onready var mail_barriere = $ColorRect
 @onready var indhold = $Indhold
 @onready var boss = $Boss
 @onready var speechbobble = $SpeechBobble
 @onready var start_button = $StartButton
 @onready var godkend_button = $GodkendButton
 @onready var afvis_button = $AfvisButton
+@onready var mail_button = $MailButton
 @onready var start_button_grey = $StartButtonGrey
+@onready var snake = $"../../Game/Snake"
+
 
 var dialogue_lines: Array[String] = [
-	"Velkommen til din Første dag på jobbet! På jobbet skal du tjekke firmaets indkommende mails for phishing",
-	"For at starte din arbejdsdag skal du klikke på startknappen. Herefter skal du vurdere om kommende mail er ægte eller phishing.",
-	"Hovsa! det var vist forkert. Hvis du kigger i mailens domæne kan du se at det ene 'o' er erstattet af et '0'. Prøv at afvis mailen",
+	"Velkommen til din Første dag på jobbet! På jobbet skal du tjekke firmaets indkommende mails for phishing.",
+	"For at starte din arbejdsdag skal du klikke på startknappen. Herefter skal du vurdere om kommende mails er ægte eller phishing mails.",
+	"Du vil i løbet af dagen modtage mails i din indbakke. Klik på mailen, og afvis den, hvis du vurderer, at det er phishing, og videresend den, hvis det er en reel mail.",
+	"Hovsa! det var vist forkert. Hvis du kigger i mailens domæne kan du se at det ene 'o' er erstattet af et '0'. Prøv at afvise mailen.",
 	"Det var helt rigtigt! Du jo et naturtalent!"
 ]
 
@@ -26,6 +33,7 @@ var text_speed: float = 0.04
 var is_typing: bool = false
 var current_index: int = 0
 var arrow_start_pos: Vector2
+var arrow_pos_2: Vector2
 var arrow_tween: Tween
 
 func _ready() -> void:
@@ -33,9 +41,12 @@ func _ready() -> void:
 	start_button.disabled = true
 	godkend_button.disabled = true
 	afvis_button.disabled = true
+	mail_button.disabled = true
 	start_button_grey.visible = false
 	arrow_start_pos = arrow.position
 	play_dialogue(dialogue_lines[dialogue_index])
+	snake.visible = false
+	snake.process_mode = Node.PROCESS_MODE_DISABLED
 
 func play_dialogue(new_text: String) -> void:
 	full_text = new_text
@@ -58,6 +69,8 @@ func show_text() -> void:
 	
 	if dialogue_index == 1:
 		start_button.disabled = false
+	if dialogue_index == 2:
+		mail_button.disabled = false
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed:
@@ -78,12 +91,13 @@ func handle_click() -> void:
 				dialogue_index += 1
 				
 				if dialogue_index == 1:
-					show_arrow()
+					show_arrow(Vector2(1130,472))
 				
 				play_dialogue(dialogue_lines[dialogue_index])
 
-func show_arrow() -> void:
+func show_arrow(new_pos:Vector2) -> void:
 	arrow.visible = true
+	arrow_start_pos = new_pos
 	arrow.position = arrow_start_pos
 	
 	if arrow_tween:
@@ -95,36 +109,75 @@ func show_arrow() -> void:
 	arrow_tween.tween_property(arrow, "position", arrow_start_pos, 0.4)
 
 func _on_startbutton_pressed() -> void:
-	afsender.visible = true
-	emne.visible = true
-	indhold.visible = true
+	start_button_grey.visible = true
+	mail_barriere.visible = true
+	titel_mail.visible = true
+	emne_mail.visible = true
+	
+	show_arrow(Vector2(400,212))
+	
+	arrow.rotation_degrees = 180
+	
+	if arrow_tween:
+		arrow_tween.kill()
+	
+	arrow_tween = create_tween()
+	arrow_tween.set_loops()
+	arrow_tween.tween_property(arrow, "position", arrow_start_pos + Vector2(-12, 0), 0.4)
+	arrow_tween.tween_property(arrow, "position", arrow_start_pos, 0.4)
+	
+	dialogue_index = 2
+	play_dialogue(dialogue_lines[2])
+	
+	var tween = create_tween()
+	var target_position_boss = Vector2(657, 485)
+	var target_position_speechbobble = Vector2(778, 211)
+	var target_position_speech_text = Vector2(799, 234)
+	
+	tween.tween_property(boss, "position", target_position_boss, 1.0)\
+		.set_trans(Tween.TRANS_SINE)\
+		.set_ease(Tween.EASE_IN_OUT)
+	tween.parallel().tween_property(speechbobble, "position", target_position_speechbobble, 1.0)\
+		.set_trans(Tween.TRANS_SINE)\
+		.set_ease(Tween.EASE_IN_OUT)
+	tween.parallel().tween_property(speech_text, "position", target_position_speech_text, 1.0)\
+		.set_trans(Tween.TRANS_SINE)\
+		.set_ease(Tween.EASE_IN_OUT)
+		
+func _on_mail_button_pressed() -> void:
 	arrow.visible = false
 	speechbobble.visible = false
 	boss.visible = false
 	speech_text.visible = false
-	start_button_grey.visible = true
-	
+	afsender_indhold.visible = true
+	emne_indhold.visible = true
+	indhold.visible = true
+
 	godkend_button.disabled = false
 	afvis_button.disabled = false
+
+	boss.position = Vector2(197, 485)
+	speechbobble.position = Vector2(318, 211)
+	speech_text.position = Vector2(339, 234)
 	
-	
-	
+	dialogue_index = 3
+	play_dialogue(dialogue_lines[3])
+
 func _on_godkend_button_pressed() -> void:
 	speechbobble.visible = true
 	boss.visible = true
 	speech_text.visible = true
-	
-	
-	play_dialogue(dialogue_lines[2])
-	
 	godkend_button.disabled = true
 
 func _on_afvis_button_pressed() -> void:
 	speechbobble.visible = true
 	boss.visible = true
 	speech_text.visible = true
-	afsender.visible = false
-	emne.visible = false
+	afsender_indhold.visible = false
+	emne_indhold.visible = false
 	indhold.visible = false
-	
-	play_dialogue(dialogue_lines[3])
+	mail_barriere.visible = false
+	titel_mail.visible = false
+	emne_mail.visible = false
+	dialogue_index = 4
+	play_dialogue(dialogue_lines[4])
